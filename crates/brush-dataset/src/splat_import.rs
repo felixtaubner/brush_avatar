2bde1a67c64da03aa67238e6deb7936496572b3b
Felix Taubner <ftaubner@ftaubnermacbook.local>
2024-12-19 11:07:08 -0500

    implemented avatar splats

--- crates/brush-dataset/src/splat_import.rs
+++ crates/brush-dataset/src/splat_import.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use async_fn_stream::try_fn_stream;
 use brush_render::{render::rgb_to_sh, Backend};
 use burn::tensor::{Tensor, TensorData};
-use glam::{Quat, Vec3, Vec4};
+use glam::{Quat, Vec3, Vec4, UVec3, Mat3};
 use ply_rs::{
     parser::Parser,
     ply::{ElementDef, Header, Property, PropertyAccess},
@@ -20,6 +20,7 @@ pub(crate) struct GaussianData {
     pub(crate) log_scale: Vec3,
     pub(crate) opacity: f32,
     pub(crate) rotation: Quat,
+    pub(crate) binding: u32,
     pub(crate) sh_dc: [f32; 3],
     // NB: This is in the inria format, aka [channels, coeffs]
     // not [coeffs, channels].
@@ -32,6 +33,7 @@ impl PropertyAccess for GaussianData {
             means: Vec3::ZERO,
             log_scale: Vec3::ZERO,
             opacity: 0.0,
+            binding: 0,
             rotation: Quat::IDENTITY,
             sh_dc: [0.0, 0.0, 0.0],
             sh_coeffs_rest: Vec::new(),
@@ -74,6 +76,7 @@ impl PropertyAccess for GaussianData {
             b"red" => self.sh_dc[0] = rgb_to_sh(value),
             b"green" => self.sh_dc[1] = rgb_to_sh(value),
             b"blue" => self.sh_dc[2] = rgb_to_sh(value),
+            b"binding" => self.binding = value as u32,
             _ if key.starts_with("f_rest_") => {
                 if let Ok(idx) = key["f_rest_".len()..].parse::<u32>() {
                     if idx >= self.sh_coeffs_rest.len() as u32 {
@@ -104,6 +107,7 @@ impl PropertyAccess for GaussianData {
             b"f_dc_0" => Some(self.sh_dc[0]),
             b"f_dc_1" => Some(self.sh_dc[1]),
             b"f_dc_2" => Some(self.sh_dc[2]),
+            b"binding" => Some(self.binding as f32),
             _ if key.starts_with("f_rest_") => {
                 if let Ok(idx) = key["f_rest_".len()..].parse::<usize>() {
                     self.sh_coeffs_rest.get(idx).copied()
@@ -112,6 +116,35 @@ impl PropertyAccess for GaussianData {
                 }
             }
             _ => None,
+        }
+    }
+}
+
+pub(crate) struct FaceData {
+    pub(crate) indices: UVec3,
+}
+
+impl PropertyAccess for FaceData {
+    fn new() -> Self {
+        FaceData {
+            indices: UVec3::ZERO,
+        }
+    }
+
+    fn set_property(&mut self, key: &str, property: Property) {
+        let ascii = key.as_bytes();
+
+        let mut value = if let Property::UInt(value) = property {
+            value
+        } else {
+            return;
+        };
+
+        match ascii {
+            b"index_0" => self.indices[0] = value,
+            b"index_1" => self.indices[1] = value,
+            b"index_2" => self.indices[2] = value,
+            _ => (),
         }
     }
 }
@@ -154,6 +187,64 @@ async fn decode_splat<T: AsyncBufRead + Unpin + 'static>(
     }
 }
 
+async fn decode_face<T: AsyncBufRead + Unpin + 'static>(
+    reader: &mut T,
+    parser: &Parser<FaceData>,
+    header: &Header,
+    element: &ElementDef,
+) -> tokio::io::Result<FaceData> {
+    match header.encoding {
+        ply_rs::ply::Encoding::Ascii => {
+            let mut ascii_line = String::new();
+            reader.read_line(&mut ascii_line).await?;
+            let elem = parser.read_ascii_element(&ascii_line, element)?;
+            ascii_line.clear();
+            Ok(elem)
+        }
+        ply_rs::ply::Encoding::BinaryBigEndian => {
+            parser.read_big_endian_element(reader, element).await
+        }
+        ply_rs::ply::Encoding::BinaryLittleEndian => {
+            parser.read_little_endian_element(reader, element).await
+        }
+    }
+}
+
+fn transform_splat(
+    vertices: &Vec<Vec3>,
+    faces: &Vec<UVec3>,
+    mean: Vec3,
+    rotation: Quat,
+    log_scale: Vec3,
+    binding: u32,
+) -> (Vec3, Quat, Vec3) {
+    // The following code is translated from GaussianAvatars: https://github.com/ShenhanQian/GaussianAvatars
+
+    let face = faces[binding as usize];
+    let v0 = vertices[face[0] as usize];
+    let v1 = vertices[face[1] as usize];
+    let v2 = vertices[face[2] as usize];
+
+    let a0 = ((v1 - v0) + 1e-20).normalize();
+    let a1 = (a0.cross(v2 - v0) + 1e-20).normalize();
+    let a2 = (-a1.cross(a0) + 1e-20).normalize();
+
+    let orientation: Mat3 = Mat3::from_cols(a0, a1, a2);
+
+    let s0 = (v1 - v0).length();
+    let s1 = a2.dot(v2 - v0).abs();
+    let scale = (s0 + s1) / 2.0;
+
+    let face_center = (v0 + v1 + v2) / 3.0;
+    let orient_quat: Quat = Quat::from_mat3(&orientation).normalize();
+
+    let new_mean = (orientation * mean) * scale + face_center;
+    let new_rot = orient_quat * rotation.normalize();
+    let new_scale = log_scale + scale.ln();
+
+    (new_mean, new_rot, new_scale)
+}
+
 pub struct SplatMetadata {
     pub up_axis: Vec3,
     pub total_splats: usize,
@@ -186,6 +277,7 @@ pub fn load_splat_from_ply<T: AsyncRead + Unpin + 'static, B: Backend>(
 
     try_fn_stream(|emitter| async move {
         let gaussian_parser = Parser::<GaussianData>::new();
+        let face_parser = Parser::<FaceData>::new();
 
         let header = gaussian_parser.read_header(&mut reader).await?;
 
@@ -210,16 +302,16 @@ pub fn load_splat_from_ply<T: AsyncRead + Unpin + 'static, B: Backend>(
         let mut final_splat = None;
         let mut frame = 0;
 
-        let mut meta_min = QuantMeta {
-            mean: Vec3::ZERO,
-            rotation: Vec4::ZERO,
-            scale: Vec3::ZERO,
-        };
-        let mut meta_max = QuantMeta {
-            mean: Vec3::ONE,
-            rotation: Vec4::ONE,
-            scale: Vec3::ONE,
-        };
+        let mut meta_min = Vec3::ZERO;
+        let mut meta_max = Vec3::ONE;
+        let mut faces: Option<Vec<UVec3>> = None;
+        let mut base_vertices: Option<Vec<Vec3>> = None;
+
+        let mut base_means: Option<Vec<Vec3>> = None;
+        let mut base_rotations: Option<Vec<Quat>> = None;
+        let mut base_scales: Option<Vec<Vec3>> = None;
+        let mut base_bindings: Option<Vec<u32>> = None;
+
         for element in &header.elements {
             let properties: HashSet<_> =
                 element.properties.iter().map(|x| x.name.clone()).collect();
@@ -248,10 +340,35 @@ pub fn load_splat_from_ply<T: AsyncRead + Unpin + 'static, B: Backend>(
                 .contains("opacity")
                 .then(|| Vec::with_capacity(element.count));
 
-            if element.name == "vertex" {
+            if element.name == "faces" {
+                faces = Some(Vec::with_capacity(element.count));
+                for _i in 0..element.count {
+                    let face =
+                        decode_face(&mut reader, &face_parser, &header, element).await?;
+                    if let Some(ref mut fcs) = faces {
+                        fcs.push(face.indices)
+                    }
+                    // faces.expect("REASON").push(face.indices);
+                }
+            } else if element.name == "base_vertex" {
+                base_vertices = Some(Vec::with_capacity(element.count));
+                for _i in 0..element.count {
+                    let vert =
+                        decode_splat(&mut reader, &gaussian_parser, &header, element).await?;
+                    if let Some(ref mut vrts) = base_vertices {
+                        vrts.push(vert.means)
+                    }
+                    // base_vertices.expect("REASON").push(vert.means);
+                }
+            } else if element.name == "vertex" {
                 if ["x", "y", "z"].into_iter().any(|p| !properties.contains(p)) {
                     Err(anyhow::anyhow!("Invalid splat ply. Missing properties!"))?
                 }
+
+                base_means = Some(Vec::with_capacity(element.count));
+                base_rotations = Some(Vec::with_capacity(element.count));
+                base_scales = Some(Vec::with_capacity(element.count));
+                base_bindings = Some(Vec::with_capacity(element.count));
 
                 for i in 0..element.count {
                     // Ocassionally yield.
@@ -294,21 +411,34 @@ pub fn load_splat_from_ply<T: AsyncRead + Unpin + 'static, B: Backend>(
                     let splat =
                         decode_splat(&mut reader, &gaussian_parser, &header, element).await?;
 
-                    means.push(splat.means);
-                    if let Some(scales) = log_scales.as_mut() {
-                        scales.push(splat.log_scale);
-                    }
-                    if let Some(rotation) = rotations.as_mut() {
-                        rotation.push(splat.rotation.normalize());
-                    }
-                    if let Some(opacity) = opacity.as_mut() {
-                        opacity.push(splat.opacity);
-                    }
-                    if let Some(sh_coeffs) = sh_coeffs.as_mut() {
-                        let sh_coeffs_interleaved =
-                            interleave_coeffs(splat.sh_dc, &splat.sh_coeffs_rest);
-                        sh_coeffs.extend(sh_coeffs_interleaved);
-                    }
+                    // Save local transform and rotation
+                    base_means.as_mut().unwrap().push(splat.means);
+                    base_rotations.as_mut().unwrap().push(splat.rotation);
+                    base_scales.as_mut().unwrap().push(splat.log_scale);
+                    base_bindings.as_mut().unwrap().push(splat.binding);
+
+                    let rig_faces = faces.as_ref().ok_or_else(|| anyhow::anyhow!("Invalid splat ply. Missing properties!"))?;
+                    let rig_vertices = base_vertices.as_ref().ok_or_else(|| anyhow::anyhow!("Invalid splat ply. Missing properties!"))?;
+
+                    let (mean, rot, log_scale) = transform_splat(
+                        rig_vertices,
+                        rig_faces,
+                        splat.means,
+                        splat.rotation,
+                        splat.log_scale,
+                        splat.binding,
+                    );
+
+                    means.push(mean);
+
+                    log_scales.as_mut().unwrap().push(log_scale);
+                    rotations.as_mut().unwrap().push(rot.normalize());
+
+                    opacity.as_mut().unwrap().push(splat.opacity);
+                    sh_coeffs
+                        .as_mut()
+                        .unwrap()
+                        .extend(interleave_coeffs(splat.sh_dc, &splat.sh_coeffs_rest));
                 }
 
                 let splats =
@@ -327,76 +457,68 @@ pub fn load_splat_from_ply<T: AsyncRead + Unpin + 'static, B: Backend>(
                     .await;
             } else if element.name.starts_with("meta_delta_min_") {
                 let splat = decode_splat(&mut reader, &gaussian_parser, &header, element).await?;
-                meta_min.mean = splat.means;
-                meta_min.rotation = splat.rotation.into();
-                meta_min.scale = splat.log_scale;
+                meta_min = splat.means;
             } else if element.name.starts_with("meta_delta_max_") {
                 let splat = decode_splat(&mut reader, &gaussian_parser, &header, element).await?;
-                meta_max.mean = splat.means;
-                meta_max.rotation = splat.rotation.into();
-                meta_max.scale = splat.log_scale;
+                meta_max = splat.means;
             } else if element.name.starts_with("delta_vertex_") {
                 let Some(splats) = final_splat.clone() else {
                     anyhow::bail!("Need to read base splat first.");
                 };
+                let base_mns = base_means.as_ref().unwrap();
+                let base_rot = base_rotations.as_ref().unwrap();
+                let base_scl = base_scales.as_ref().unwrap();
+                let base_bind = base_bindings.as_ref().unwrap();
+
+                let mut new_vertices: Vec<Vec3> = Vec::with_capacity(element.count);
 
                 for i in 0..element.count {
                     // Ocassionally yield.
                     if i % 500 == 0 {
                         tokio::task::yield_now().await;
                     }
-                    // The splat we decode is normed to 0-1 (if quantized), so rescale to
-                    // actual values afterwards.
                     let splat_enc =
                         decode_splat(&mut reader, &gaussian_parser, &header, element).await?;
 
-                    // Let's only animate transforms for now.
-                    means.push(splat_enc.means * (meta_max.mean - meta_min.mean) + meta_min.mean);
-
-                    if let Some(rotation) = rotations.as_mut() {
-                        let val: Vec4 = splat_enc.rotation.into();
-                        let val = val * (meta_max.rotation - meta_min.rotation) + meta_min.rotation;
-                        rotation.push(Quat::from_vec4(val));
-                    }
-
-                    if let Some(log_scales) = log_scales.as_mut() {
-                        log_scales.push(
-                            splat_enc.log_scale * (meta_max.scale - meta_min.scale)
-                                + meta_min.scale,
-                        );
-                    }
+                    // The splat we decode is normed to 0-1 (if quantized), so rescale to
+                    // actual values afterwards.
+                    if let Some(ref rig_vertices) = base_vertices {
+                        new_vertices.push(splat_enc.means * (meta_max - meta_min) + meta_min + rig_vertices[i]);
+                    } else {
+                        Err(anyhow::anyhow!("Invalid splat ply. Missing properties!"))?
+                    }   
                     // Don't emit any intermediate states as it looks strange to have a torn state.
+                }
+;
+                let mut new_means: Vec<Vec3> = Vec::with_capacity(base_mns.len());
+                let mut new_rots: Vec<Quat> = Vec::with_capacity(base_rot.len());
+                let mut new_scales: Vec<Vec3> = Vec::with_capacity(base_scl.len());
+
+                for i in 0..base_mns.len() {
+                    if let Some(ref rig_faces) = faces {
+                        let (mean, rot, log_scale) = 
+                            transform_splat(&new_vertices, &rig_faces, base_mns[i], base_rot[i], base_scl[i], base_bind[i]);
+                        new_means.push(mean);
+                        new_rots.push(rot);
+                        new_scales.push(log_scale);
+                    } else {
+                        Err(anyhow::anyhow!("Invalid splat ply. Missing properties!"))?
+                    }
                 }
 
                 let n_splats = splats.num_splats();
-                let means_tensor: Vec<f32> = means.iter().flat_map(|v| [v.x, v.y, v.z]).collect();
-                let means =
-                    Tensor::from_data(TensorData::new(means_tensor, [n_splats, 3]), &device)
-                        + splats.means.val();
-
-                // The encoding is just delta encoding in floats - nothing fancy
-                // like actually considering the quaternion transform.
-                let rotations = if let Some(rotations) = rotations {
-                    let rotations: Vec<f32> = rotations
-                        .into_iter()
-                        .flat_map(|v| [v.w, v.x, v.y, v.z])
-                        .collect();
-                    Tensor::from_data(TensorData::new(rotations, [n_splats, 4]), &device)
-                        + splats.rotation.val()
-                } else {
-                    splats.rotation.val()
-                };
-
-                let log_scales = if let Some(log_scales) = log_scales {
-                    let log_scales: Vec<f32> = log_scales
-                        .into_iter()
-                        .flat_map(|v| [v.x, v.y, v.z])
-                        .collect();
-                    Tensor::from_data(TensorData::new(log_scales, [n_splats, 3]), &device)
-                        + splats.log_scales.val()
-                } else {
-                    splats.log_scales.val()
-                };
+                let means = Tensor::from_data(
+                    TensorData::new(new_means.iter().flat_map(|v| [v.x, v.y, v.z]).collect(), [n_splats, 3]), 
+                    &device
+                );
+                let rotations = Tensor::from_data(
+                    TensorData::new(new_rots.into_iter().flat_map(|v| [v.w, v.x, v.y, v.z]).collect(), [n_splats, 4]),
+                    &device
+                );
+                let log_scales = Tensor::from_data(
+                    TensorData::new(new_scales.into_iter().flat_map(|v| [v.x, v.y, v.z]).collect(), [n_splats, 3]),
+                    &device
+                );
 
                 let mut new_splat = Splats::from_tensor_data(
                     means,
